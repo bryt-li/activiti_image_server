f40cdec58414681b1d87635f0a36a93682225aaf
Bryt Li <bryt@oriente.com>
2018-03-02 09:37:45 +0800

    update

--- src/main/java/li/bryt/tools/activiti_image_server/modules/utils/baseModule.java
+++ src/main/java/li/bryt/tools/activiti_image_server/modules/utils/baseModule.java
@@ -1,6 +1,5 @@
 package li.bryt.tools.activiti_image_server.modules.utils;
 
-import org.nutz.json.Json;
 import org.nutz.lang.util.NutMap;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
@@ -10,19 +9,15 @@ public abstract class baseModule {
 	protected final Log LOG = Logs.getLog(this.getClass());
 
 	protected RESTfulResponse ok(NutMap data) {
-		LOG.debugf("return ok='%s'", Json.toJson(data));
 		return new RESTfulResponse(true, data);
 	}
 		
 	protected RESTfulResponse err(String errmsg){
-		LOG.debugf("return err='%s'", errmsg);
-		return new RESTfulResponse(false, new NutMap().setv("errmsg", errmsg));
+		return new RESTfulResponse(false, new NutMap().setv("error", errmsg));
 	}
 	
 	protected RESTfulResponse err(Exception e){
-		String errmsg = e.getMessage();
-		LOG.debugf("return err='%s'", errmsg);
-		return new RESTfulResponse(false, new NutMap().setv("errmsg", errmsg));
+		return new RESTfulResponse(false, new NutMap().setv("error", e.getMessage()));
 	}
 
 }
